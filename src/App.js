8e57ec8459066a84b875e1f7c255cc4f99e3758a
Jannat-Ara <jannat.raya@northsouth.edu>
2020-10-24 14:59:18 +0600

    routing part

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 import Courses from './Components/Courses/Courses';
 import Home from './Components/Home/Home';
 import Header from './Header/Header';
+import {BrowserRouter as Router,Route} from 'react-router-dom';
+
+
+
 // variable for courses
 const courses=[{name:'Data Science : Complete Alteryx Bootcamp',instructor:'Dr. Shazzad Hosain' , rating:'4.9 ⭐⭐⭐⭐⭐', price:'$12.00'},
 {name:'Data Science and Machine Learning with Python and Libraries', instructor:'Dr. Mohammad Rashedur Rahman', rating:'4.8 ⭐⭐⭐⭐⭐',price:'$11.99'},
-{name:'Complete Outlier Detection Algorithms A-Z: In Data Science', instructor:'Dr. Nova Ahmed',rating:'4.9 ⭐⭐⭐⭐⭐',price:'$11.88'},
+{name:'Complete Outlier Detection Algorithms A-Z: In Data Science', instructor:'Dr. Nova Ahmed',rating:'4.9 ⭐⭐⭐⭐⭐',price:'11.88'},
 {name:'The Complete Tableau Bootcamp for Data Visualization', instructor:'Dr. Tanzilur Rahman',rating:'4.8 ⭐⭐⭐⭐⭐',price:'$10.99'},
 {name:'Feature Selection for Machine Learning', instructor:'Dr. Md. Mahfuzur Rahman',rating:'4.8 ⭐⭐⭐⭐⭐',price:'$12.99'},
 {name:'2020 Complete Python Bootcamp: From Zero to Hero in Python', instructor:'Dr. Lamia Iftekhar',rating:'4.7 ⭐⭐⭐⭐⭐',price:'$10.99'}
@@ -17,10 +21,18 @@ const courses=[{name:'Data Science : Complete Alteryx Bootcamp',instructor:'Dr. 
 function App() {
   return (
     <div>
-     <Header></Header>
-     <Home></Home>
-     <h2>Courses</h2>
-     <ul>{courses.map(courses =><Courses courses={courses}></Courses>)}</ul>
+       <Router>
+         <Header></Header>
+          <Route path="/home" component={Home}>
+            <Home></Home>
+          </Route>
+          <Route path="/explore_courses" component={Courses}>
+          <h2>Courses</h2>
+          <ul>{courses.map(courses =><Courses courses={courses}></Courses>)}</ul>
+          </Route>
+      </Router> 
+     {/* <h2>Courses</h2>
+     <ul>{courses.map(courses =><Courses courses={courses}></Courses>)}</ul> */}
     </div>
   );
 }
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,16 +1,41 @@
 import React from 'react';
-import './Header.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee ,faHeart,faCartArrowDown, faUser, faHome, faChalkboardTeacher} from '@fortawesome/free-solid-svg-icons'
+import {Switch,Link} from 'react-router-dom';
+import './Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee ,faHeart,faCartArrowDown, faUser, faHome, faChalkboardTeacher} from '@fortawesome/free-solid-svg-icons';
+
+
 const Header = () => {
     return (
         <div className="header">
             <nav>
-                <a href="/home">Home <FontAwesomeIcon icon={faHome} /></a>
-                <a href="/tutors">Tutors <FontAwesomeIcon icon={faChalkboardTeacher} /></a>
+                <Switch>
+                    <Link to="/home"><a>Home <FontAwesomeIcon icon={faHome} /></a>  
+                    </Link>
+                </Switch>
+                <Switch>
+                    <Link to="/tutors"><a>Tutors <FontAwesomeIcon icon={faChalkboardTeacher} /></a>
+                    </Link>
+                </Switch>
+                <Switch>
+                    <Link to="/explore_courses"><a>Explore Courses <FontAwesomeIcon icon={faHeart} /></a>
+                    </Link>
+                </Switch>
+                <Switch>
+                    <Link to="/myProfile"><a>My Profile <FontAwesomeIcon icon={faUser} /></a> 
+                    </Link>
+                </Switch>
+                <Switch>
+                    <Link to="/cart"><a>Cart<FontAwesomeIcon icon={faCartArrowDown} /></a>
+                    </Link>
+                </Switch>
+                
+
+                               
+                {/* <a href="/tutors">Tutors <FontAwesomeIcon icon={faChalkboardTeacher} /></a>
                 <a href="/explore_courses">Explore Courses <FontAwesomeIcon icon={faHeart} /></a>
                 <a href="/cart">Cart<FontAwesomeIcon icon={faCartArrowDown} /></a>
-                <a href="/myProfile">My Profile <FontAwesomeIcon icon={faUser} /></a>
+                <a href="/myProfile">My Profile <FontAwesomeIcon icon={faUser} /></a> */}
             </nav>
         </div>
     );
